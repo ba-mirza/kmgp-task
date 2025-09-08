2b724d6bcdc42486917f45935ee786a603c9f753
dev <almas1610@gmail.com>
2025-09-08 16:15:09 +0500

    [features]: add listbox as autocomplete

--- src/app/pages/orders/orders.ts
+++ src/app/pages/orders/orders.ts
@@ -2,11 +2,12 @@ import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import {OrdersApiService} from '../../core/api/orders-api.service';
 import { TagModule } from 'primeng/tag';
 import {TableModule} from 'primeng/table';
-import {Order} from '../../core/types';
-import {debounceTime, distinctUntilChanged, map, Observable, of, startWith, switchMap} from 'rxjs';
+import {Order, OrderStatus} from '../../core/types';
+import {debounceTime, distinctUntilChanged, map, Observable, of, shareReplay, startWith, switchMap, tap} from 'rxjs';
 import {AsyncPipe} from '@angular/common';
-import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {InputComponent} from '../../components/ui/input.component';
+import { ListboxModule } from 'primeng/listbox';
 
 @Component({
   selector: 'app-orders',
@@ -16,26 +17,28 @@ import {InputComponent} from '../../components/ui/input.component';
     AsyncPipe,
     InputComponent,
     ReactiveFormsModule,
+    ListboxModule,
+    FormsModule,
   ],
   templateUrl: './orders.html',
-  styleUrl: './orders.scss'
 })
 export class Orders implements OnInit {
   protected orderService = inject(OrdersApiService);
+
   protected orderList = signal<Observable<Order[]>>(of([]));
+  protected show = signal<boolean>(false);
+  protected statuses = signal<{status: OrderStatus}[]>([]);
+  protected selectedStatus: OrderStatus = 'new';
 
   protected formattedDate = (date: string) => new Date(date).toDateString();
 
-  protected searchForms = new FormGroup({
-    customerName: new FormControl(''),
-    status: new FormControl(''),
-  })
+  protected customerName = new FormControl('')
 
   protected paginatorState = computed(() => (this.filteredOrders.length > 10));
   protected scrollableState = computed(() => (this.filteredOrders.length > 20))
 
   protected filteredOrders = computed((): Observable<Order[]> => {
-    const control = this.searchForms.controls.customerName as FormControl;
+    const control = this.customerName as FormControl;
 
     const nameChanges$ = control.valueChanges
       ? (control.valueChanges as Observable<string>).pipe(
@@ -52,14 +55,25 @@ export class Orders implements OnInit {
             const q = (search ?? '').toString().toLowerCase().trim();
             if (!q) return orders;
             return orders.filter(o => (o.customerName ?? '').toLowerCase().includes(q));
-          })
+          }),
+          shareReplay(1),
         )
       )
     );
   });
 
+  protected autoComplete(flag: boolean) {
+    this.show.set(flag);
+  }
+
   ngOnInit(): void {
     const orders = this.orderService.getOrders();
-    this.orderList.set(orders)
+    this.statuses.set([
+      {status: 'new'},
+      {status: 'cancelled'},
+      {status: 'completed'},
+      {status: 'processing'},
+    ]);
+    this.orderList.set(orders);
   }
 }
